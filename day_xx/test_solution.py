21daddb665b53de7d71c7075d0b5230df81fe162
Paul Wildenhain <wildenhaip@chop.edu>
2021-12-02 15:26:06 -0500

    :recycle: Add template dir

---
+++ day_xx/test_solution.py
@@ -0,0 +1,20 @@
+# pylint: disable=missing-module-docstring,missing-function-docstring,redefined-outer-name
+
+import pytest
+
+from day_xx.solution import Something
+
+
+@pytest.fixture
+def puzzle_input():
+    return """"""
+
+
+def test_solution_part_1(puzzle_input):
+    something = Something(puzzle_input)
+    assert something
+
+
+def test_solution_part_2(puzzle_input):
+    something = Something(puzzle_input)
+    assert something
